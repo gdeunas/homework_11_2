bf8cbc0d9552f0b68fdfb1513c88ed7d02bd4999
aidar <anged@mail.ru>
2025-11-12 17:48:52 +0300

    add decorators and test/

---
+++ src/decorators.py
@@ -0,0 +1,29 @@
+from functools import wraps
+
+
+def log(filename):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if filename:
+                msg = f"{func.__name__} {result}\n" + f"{func.__name__} ok\n"
+                with open(filename, "a") as file:
+                    file.write(msg)
+            else:
+                msg = f"my_function error: {ValueError}: {args}, {kwargs}"
+                raise ValueError(msg)
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    """Функция суммирования my_function(x + y)"""
+    return x + y
+
+
+my_function(1, 2)
